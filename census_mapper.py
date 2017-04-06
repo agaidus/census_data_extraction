ab27aa78eca9a0e312e2ec9bb185ab972ed9d158
Andrew Gaidus <andrew.gaidus@gmail.com>
2017-04-06 16:56:03 -0700

    updated

--- census_mapper.py
+++ census_mapper.py
@@ -1,42 +1,64 @@
-import urllib2
-import zipfile
-import StringIO
+from urllib2 import urlopen
+from zipfile import ZipFile
+from StringIO import StringIO
 import shapefile
 import geopandas as gpd
 from shapely.geometry import shape  
 import osr
 import pandas as pd
 import requests
+from shapely.geometry import Point
+from numpy.random import RandomState, uniform
+
+
+def gen_random_points_poly(poly, num_points, seed = None):
+    min_x, min_y, max_x, max_y = poly.bounds
+    points = []
+    i=0
+    while len(points) < num_points:
+        s=RandomState(seed+i) if seed else RandomState(seed)
+        random_point = Point([s.uniform(min_x, max_x), s.uniform(min_y, max_y)])
+        if random_point.within(poly):
+            points.append(random_point)
+        i+=1
+    return points
+
+
+def num_points_in_gdf(geometry, values, points_per_value = None, seed = None):
+    
+    if points_per_value:
+        new_values = (values/points_per_value).astype(int)
+    else:
+        new_values = values
+    new_values = new_values[new_values>0]
+    g = gpd.GeoDataFrame(data = {'vals':new_values}, geometry = geometry)
+    
+    a = g.apply(lambda row: gen_random_points_poly(row['geometry'], row['vals'], seed),1)
+    b = gpd.GeoSeries(a.apply(pd.Series).stack(), crs = geometry.crs)
+    return b
+
 
 def zip_shp_to_gdf(zip_file_name):
 
-    cloudfile = urllib2.urlopen(zip_file_name)
-    memoryfile = StringIO.StringIO(cloudfile.read())
-    zipshape = zipfile.ZipFile(memoryfile)
+    zipfile = ZipFile(StringIO(urlopen(zip_file_name).read()))
+    filenames = [y for y in sorted(zipfile.namelist()) for ending in ['dbf', 'prj', 'shp', 'shx']\
+                 if y.endswith(ending)] 
+    dbf, prj, shp, shx = [StringIO(zipfile.read(filename)) for filename in filenames]
+    r = shapefile.Reader(shp=shp, shx=shx, dbf=dbf)
     
-    filenames = [y for y in sorted(zipshape.namelist()) for ending in ['dbf', 'prj', 'shp', 'shx'] if y.endswith(ending)] 
-    dbf, prj, shp, shx = [StringIO.StringIO(zipshape.read(filename)) for filename in filenames]
-    
-    reader = shapefile.Reader(shp=shp, shx=shx, dbf=dbf)
-    
-    proj4 = osr.SpatialReference(prj.read()).ExportToProj4()
-    
+    attributes, geometry = [], []
+    field_names = [field[0] for field in r.fields[1:]]  
+    for row in r.shapeRecords():  
+        geometry.append(shape(row.shape.__geo_interface__))  
+        attributes.append(dict(zip(field_names, row.record)))  
         
-    atts, shapes = [], []
-    field_names = [field[0] for field in reader.fields[1:]]  
-    for sr in reader.shapeRecords():  
-        shapely_geo = shape(sr.shape.__geo_interface__)  
-        att = dict(zip(field_names, sr.record))  
-        atts.append(att)
-        shapes.append(shapely_geo)
-    
-    gdf = gpd.GeoDataFrame(data = atts, geometry = shapes, crs = proj4)
+    proj4_string = osr.SpatialReference(prj.read()).ExportToProj4()
+    gdf = gpd.GeoDataFrame(data = attributes, geometry = geometry, crs = proj4_string)
     return gdf
 
 
 
-
-def get_census_data(year, dataset, geography, area, variables, variable_labels = None):
+def get_census_variables(year, dataset, geography, area, variables, variable_labels = None):
     
     base_url = 'https://api.census.gov/data/{}/{}'.format(year, dataset)
     
